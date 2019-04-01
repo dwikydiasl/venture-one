930908f03eb4ac7426a857029f78ec60b6ff745d
Dwiky Dias Laksmana <34709013+dwikydiasl@users.noreply.github.com>
2019-04-01 09:43:49 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Chapter One</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">  
+	<link rel="stylesheet" href="css/bootstrap.css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<!-- Fontawesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+	<div id="home">
+		<!--Menu Mobile-->
+		<div class="container">
+			<div class="menu-mobile">
+				<div class="row">
+					<div class="col-4 col-sm-4 col-md-3 col-lg-3">
+						<div class="logo-moblie">
+							<img src="assets/img/brand.png" width="100%">
+						</div>						
+					</div>
+					<div class="col-8 col-sm-8 col-md-9 col-lg-9" align="right">
+						<i class="fas fa-bars" onclick="openNav()"></i>
+					</div>
+				</div>
+			</div>				
+		</div>		
+		<div class="logo-brand" align="center">
+			<img src="assets/img/brand.png" width="100%">
+		</div>
+		<div class="container">
+			<div class="row konten-home">
+				<div class="col-12 col-sm-12 col-md-5 col-lg-5">
+					<div class="menu">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">About Us</a></li>
+							<li><a href="">Our Portofolio</a></li>
+							<li><a href="">Investment Strategy</a></li>
+							<li><a href="">The Impact</a></li>
+							<li><a href="">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-12 col-sm-12 col-md-7 col-lg-7">
+					<div class="konten-home-right">
+						<h1>Produces world-class</h1>
+						<h1>Digital companies</h1>
+						<br><br>
+						<div class="btn-outline-white">
+							<a class="btn" href="#" role="button">Learn More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="myNav" class="overlay">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<div class="overlay-content">
+			<a href="#">Home</a>
+			<a href="#">About Us</a>
+			<a href="#">Our Portofolio</a>
+			<a href="#">Investment Strategy</a>
+			<a href="#">The Impact</a>
+			<a href="#">Contact Us</a>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,304 @@
+<?php
+include "header.php";
+?>
+
+<div id="about">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-6 col-lg-6 about-left">
+				<div class="container ">
+					<div class="konten-about">
+						<div class="row title-about">
+							<div class="col-6 col-sm-6 col-md-6 title-about-left">
+								<h1>About</h1>
+								<h1>Us</h1>
+							</div>
+							<div class="col-6 col-sm-6 col-md-6">
+								<img src="assets/img/quotes.png">
+							</div>
+						</div>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+						<div class="btn-outline-blue">
+							<a class="btn" href="#" role="button">Learn More</a>
+						</div>
+					</div>						
+				</div>
+
+			</div>
+			<div class="col-12 col-sm-6 col-md-6 col-lg-6 about-right">
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="portofolio">
+	<div class="container">
+		<h1 align="center">Our Portofolio</h1>
+		<div class="row">
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/alamat.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/alamat2.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/gelora1.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/gelora.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/kreavi2.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/kreavi.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/linear2.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/linear.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/offgrid2.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/offgrid.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="img-brand">
+					<img src="assets/img/icon/wahyoo2.png" alt="Avatar" class="image">
+					<div class="overlay2">
+						<div class="img-brand2">
+							<img src="assets/img/icon/wahyoo.png">
+							<div class="btn-outline-blue2">
+								<a class="btn" href="#" role="button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+<div id="investment">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-6 col-lg-6 investment-left">
+
+			</div>
+			<div class="col-12 col-sm-6 col-md-6 col-lg-6 investment-right">
+				<div class="container ">
+					<div class="konten-investment">
+						<div class="title-investment">
+							<h1>Investment</h1>
+							<h1>Strategy</h1>
+						</div>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+						<div class="btn-outline-blue">
+							<a class="btn" href="#" role="button">Learn More</a>
+						</div>
+					</div>				
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div id="contact">
+	<div class="container">
+		<div class="row konten-contact">
+			<div class="col-12 col-sm-12 col-sm-6 col-md-6">
+				<div class="konten-kontak-left">
+					<h1>Contact</h1>
+					<h1>Us</h1>
+					<br>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<div class="row info-contact">
+						<div class="col-12 col-sm-6 col-md-6">
+							<div class="row spacing">
+								<div class="col-2 col-sm-2 col-md-2">
+									<i class="fas fa-map-marker-alt fa-2x"></i>
+								</div>
+								<div class="col-10 col-sm-10 col-md-10">
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-sm-6 col-md-6">
+							<div class="row spacing">
+								<div class="col-2 col-sm-2 col-md-2">
+									<i class="far fa-clock fa-2x"></i>
+								</div>
+								<div class="col-10 col-sm-10 col-md-10">
+									<p>
+										We are open from<br>
+										Monday - Friday<br>
+										8am - 5pm
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12 col-sm-6 col-md-6">
+							<div class="row">
+								<div class="col-2 col-sm-2 col-md-2">
+									<i class="far fa-envelope fa-2x"></i>
+								</div>
+								<div class="col-10 col-sm-10 col-md-10" style="padding-top: 5px">
+									<p>
+										venture@company.com
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-sm-6 col-md-6">
+							<div class="row">
+								<div class="col-2 col-sm-2 col-md-2">
+									<i class="fas fa-phone-volume fa-2x"></i>
+								</div>
+								<div class="col-10 col-sm-10 col-md-10" style="padding-top: 5px">
+									<p>
+										0351-000-111
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>						
+			</div>
+			<div class="col-12 col-sm-12 col-sm-6 col-md-6">
+				<div class="form-message">
+					<form>
+						<div class="row">
+							<div class="col-6 col-sm-6 col-md-6">
+								<input type="text" class="form-control" id="" aria-describedby="" placeholder="Frist Name">
+							</div>
+							<div class="col-6 col-sm-6 col-md-6">
+								<input type="text" class="form-control" id="" aria-describedby="" placeholder="Last Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<input type="email" class="form-control" id="" aria-describedby="emailHelp" placeholder="Your Email Address">
+						</div>
+						<div class="form-group">
+							<textarea class="form-control" id="" rows="6" placeholder="Leave Your Message Here"></textarea>
+						</div>
+						<br>
+						<div class="btn-outline-white">
+							<button type="button" class="btn ">Learn More</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-4 col-md-4 col-lg-4">
+				<div class="footer-brand">
+					<img src="assets/img/brand.png">
+				</div>					
+			</div>
+			<div class="col-12 col-sm-4 col-md-4 col-lg-4">
+				<div class="footer-menu">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">About Us</a></li>
+						<li><a href="">The People</a></li>
+						<li><a href="">Story</a></li>
+						<li><a href="">Partner</a></li>
+						<li><a href="">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-12 col-sm-4 col-md-4 col-lg-4">
+				<div class="footer-search">
+					<h6>Search</h6>
+					<div class="form-group">
+						<i class="fas fa-search"></i>
+						<input type="text" class="form-control" id="" aria-describedby="emailHelp" placeholder="">
+					</div>
+					<div class="sosmed">
+						<ul>
+							<li><a href=""></a></li>
+							<li><a href=""></a></li>
+							<li><a href=""></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+include "footer.php";
+?>
